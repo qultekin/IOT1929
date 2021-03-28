40a13cfaaec0d8d0102a091c8462d6c369f0a8df
qultekin <81469093+qultekin@users.noreply.github.com>
2021-03-28 20:03:29 +0300

    Add files via upload

---
+++ Saka_devresi.ino
@@ -0,0 +1,96 @@
+#include <Blynk.h>
+
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+  This example runs directly on NodeMCU.
+
+  Note: This requires ESP8266 support package:
+    https://github.com/esp8266/Arduino
+
+  Please be sure to select the right NodeMCU module
+  in the Tools -> Board menu!
+
+  For advanced settings please follow ESP examples :
+   - ESP8266_Standalone_Manual_IP.ino
+   - ESP8266_Standalone_SmartConfig.ino
+   - ESP8266_Standalone_SSL.ino
+
+  Change WiFi ssid, pass, and Blynk auth token to run :)
+  Feel free to apply it to any other example. It's simple!
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "h2huFjne0z5i_fJ0r2EQdWKdynVgOBXh";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "tutunamayanlar";
+char pass[] = "hDc06ec31EG32g79...pRt";
+int ledler[] {D2,D3,D4};
+
+bool buton_durumu=0;
+int buton=V1;
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+  pinMode(V1,INPUT);
+  pinMode(D4,OUTPUT);
+   pinMode(D3,OUTPUT);
+   pinMode(D2,OUTPUT);
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
+BLYNK_WRITE (V1) {
+
+ for (int i=0; i<=2;i=i+1){
+    digitalWrite(D4,HIGH);
+    delay(5000);
+    digitalWrite(D4,LOW);
+    digitalWrite(D3,HIGH);
+     delay(5000);
+     digitalWrite(D3,LOW);
+    digitalWrite(D2,HIGH);
+    delay(5000);
+     digitalWrite(D2,LOW);
+ }
+}
+ 
+ 
+    
+  
+  
+  
